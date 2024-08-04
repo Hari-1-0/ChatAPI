7979c442a8eabc74186cb532ff29cbf02ef2e2bc
Hari <hariharan256633@gmail.com>
2024-08-04 15:51:19 +0530

    Backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`)
+});
